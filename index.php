de4877d2864bbbe7eb4ed398d3f5f0936a4a8051
coriawork <manuelcoriawork@gmail.com>
2023-05-30 19:06:24 -0300

    creacion de los endpoints de las plataformas

--- index.php
+++ index.php
@@ -4,19 +4,25 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require 'App/src/Models/GenerosController.php';
+require 'App/src/Models/PlataformaController.php';
 require 'vendor/autoload.php';
 require 'App/src/Models/DB.php';
 
 $app = AppFactory::create();
 
-/* $app->get('/generos/get/{genero}','\App\src\GenerosController:getGen'); */
-
-$app->get('/generos','\App\src\Models\GenerosController:list');
 
 $app->post('/generos', '\App\src\Models\GenerosController:create');
 
-$app->put('/generos', '\App\src\Models\GenerosController:update');
+$app->get('/generos','\App\src\Models\GenerosController:list');
+
+$app->put('/generos/{id}', '\App\src\Models\GenerosController:update');
 
 $app->delete('/generos', '\App\src\Models\GenerosController:delete');
+
+$app->post('/plataformas', '\App\src\Models\PlataformaController:create');
+
+$app->get('/plataformas', '\App\src\Models\PlataformaController:list');
+
+$app->put('/plataformas/{id}', '\App\src\Models\PlataformaController:update');
 
 $app->run();
