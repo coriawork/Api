f3d85ca7b5c16763ba38683853524304956ff0af
coriawork <manuelcoriawork@gmail.com>
2023-05-30 19:37:57 -0300

    endpoint del delete

--- index.php
+++ index.php
@@ -10,19 +10,16 @@ require 'App/src/Models/DB.php';
 
 $app = AppFactory::create();
 
-
+//endopoints Generos
 $app->post('/generos', '\App\src\Models\GenerosController:create');
-
 $app->get('/generos','\App\src\Models\GenerosController:list');
-
 $app->put('/generos/{id}', '\App\src\Models\GenerosController:update');
-
 $app->delete('/generos', '\App\src\Models\GenerosController:delete');
 
+//endopoints Plataformas
 $app->post('/plataformas', '\App\src\Models\PlataformaController:create');
-
 $app->get('/plataformas', '\App\src\Models\PlataformaController:list');
-
 $app->put('/plataformas/{id}', '\App\src\Models\PlataformaController:update');
+$app->delete('/plataformas/{id}', '\App\src\Models\PlataformaController:delete');
 
 $app->run();
