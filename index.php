54635bcf58591c3f17a946b69f5f81dfb6f194f3
Facundo Lubo <facundolubo@gmail.com>
2023-05-31 11:30:27 -0300

    Trabajando sobre JuegosController

--- index.php
+++ index.php
@@ -5,21 +5,24 @@ use Slim\Factory\AppFactory;
 
 require 'App/src/Models/GenerosController.php';
 require 'App/src/Models/PlataformaController.php';
+require 'App/src/Models/JuegosController.php';
 require 'vendor/autoload.php';
 require 'App/src/Models/DB.php';
 
 $app = AppFactory::create();
 
-//endopoints Generos
+//endpoints Generos
 $app->post('/generos', '\App\src\Models\GenerosController:create');
 $app->get('/generos','\App\src\Models\GenerosController:list');
 $app->put('/generos/{id}', '\App\src\Models\GenerosController:update');
 $app->delete('/generos', '\App\src\Models\GenerosController:delete');
 
-//endopoints Plataformas
+//endpoints Plataformas
 $app->post('/plataformas', '\App\src\Models\PlataformaController:create');
 $app->get('/plataformas', '\App\src\Models\PlataformaController:list');
 $app->put('/plataformas/{id}', '\App\src\Models\PlataformaController:update');
 $app->delete('/plataformas/{id}', '\App\src\Models\PlataformaController:delete');
-
+//endpoints Juegos
+//
+//
 $app->run();
