7222a33ccb974f95fb61213266e4e381f99e7dea
coriawork <manuelcoriawork@gmail.com>
2023-05-31 17:13:41 -0300

    implementacion de los formatos del DB en PDO

--- App/src/Models/PlataformaController.php
+++ App/src/Models/PlataformaController.php
@@ -2,66 +2,73 @@
 namespace App\src\Models;
 use App\src\Models\DB;
 use Exception;
-Class PlataformaController{
+
+class PlataformaController{
     public function create($request, $response, $args){
         $db = new DB();
         try{          
             $body = json_decode($request->getBody(), true);
-            if (!isset($body['nombre'])) throw new Exception("no se recibio el parametro", 400);
-            $nombre=$body['nombre'];
-            $db->makeQuery("INSERT INTO plataformas (nombre) VALUES ('" . $nombre . "')");
-            return $response->withStatus(200);
+            if (!isset($body['nombre'])) throw new Exception("no se recibió el parámetro", 400);
+            $nombre = $body['nombre'];
+            $db->makeQuery("INSERT INTO plataformas (nombre) VALUES (?)", [$nombre]);
+            $db->close();
+            return $response->withStatus(201);
         }
         catch(Exception $e) {
             $response->getBody()->write($e->getMessage());
+            $db->close();
             return $response->withStatus($e->getCode());
         }
     }
+    
     public function list($request, $response, $args){
         $db = new DB();
         try {
-            $generos = $db->makeQuery('SELECT * FROM plataformas')->fetch_all(MYSQLI_ASSOC);
-            //*Envio de Exepciones
-            if (sizeof($generos) === 0) throw new Exception("No hay generos", 404);
-
-            $response->getBody()->write(json_encode($generos));
+            $plataformas = $db->makeQuery('SELECT * FROM plataformas')->fetchAll();
+            if (count($plataformas) === 0) throw new Exception("No hay plataformas", 404);
+            $response->getBody()->write(json_encode($plataformas));
+            $db->close();
             return $response->withStatus(200);
         } catch (Exception $e) {
             $response->getBody()->write($e->getMessage());
-            return $response->withStatus($e->getCode());
+            $db->close();
+            return $response->withStatus(404);
         }
-        //todo: catch pdo exception para la conexion de la base de datos
     }
+    
     public function update($request, $response, $args){
         $db = new DB();
         try {
-            if(!is_numeric($args['id'])) throw new Exception("el id debe ser numerico", 400);
-            if(!isset($args['id'])) throw new Exception("no se recibio el id para hacer el uptdate", 400);
-            if(!$db->ExistIn('Plataformas',$args['id'])) throw new Exception("No se encontro el id: '" . $args['id'] . "'", 404);
-
+            if (!is_numeric($args['id'])) throw new Exception("el id debe ser numérico", 400);
+            if (!isset($args['id'])) throw new Exception("no se recibió el id para hacer el update", 400);
+            if (!$db->existsIn('plataformas', $args['id'])) throw new Exception("No se encontró el id: '" . $args['id'] . "'", 404);
+            
             $body = json_decode($request->getBody(), true);
-
-            if(!isset($body['nombre']))throw new Exception("no se recibio el parametro para update", 400);
-
-            $db->makeQuery("UPDATE plataformas SET nombre='" . $body['nombre'] . "' WHERE id='" . $args['id'] . "'");
+            if (!isset($body['nombre'])) throw new Exception("no se recibió el parámetro para el update", 400);
+            
+            $db->makeQuery("UPDATE plataformas SET nombre = ? WHERE id = ?", [$body['nombre'], $args['id']]);
+            $db->close();
             return $response->withStatus(200);
         } catch (Exception $e) {
             $response->getBody()->write($e->getMessage());
+            $db->close();
             return $response->withStatus($e->getCode());
         }
     }
+    
     public function delete($request, $response, $args){
         $db = new DB();
         try {
-            if(!is_numeric($args['id'])) throw new Exception("el id debe ser numerico", 400);
-            if(!isset($args['id'])) throw new Exception("no se recibio el id para hacer el uptdate", 400);
-            if(!$db->ExistIn('Plataformas',$args['id'])) throw new Exception("No se encontro el id: '". $args['id']. "'", 404);
-
-            $db->makeQuery("DELETE FROM plataformas WHERE id='". $args['id']. "'");
+            if (!is_numeric($args['id'])) throw new Exception("el id debe ser numérico", 400);
+            if (!isset($args['id'])) throw new Exception("no se recibió el id para hacer el delete", 400);
+            if (!$db->existsIn('plataformas', $args['id'])) throw new Exception("No se encontró el id: '" . $args['id'] . "'", 404);
+            
+            $db->makeQuery("DELETE FROM plataformas WHERE id = ?", [$args['id']]);
+            $db->close();
             return $response->withStatus(200);
-        }
-        catch(Exception $e){
+        } catch (Exception $e) {
             $response->getBody()->write($e->getMessage());
+            $db->close();
             return $response->withStatus($e->getCode());
         }
     }
