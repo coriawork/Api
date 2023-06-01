bc41ea0e5ffb44ad5144a1102db94fe0a87f1b24
Facundo Lubo <facundolubo@gmail.com>
2023-06-01 12:22:08 -0300

    Migracion a JuegosController

--- App/src/Models/JuegosController.php
+++ App/src/Models/JuegosController.php
@@ -4,42 +4,110 @@ use App\src\Models\DB;
 use Exception;
 
 class JuegosController{
-    //*post 201 si se creo bien
-
-    //* crea un juego
-    public function create($request, $response, $args){
+    public function juegosAll($request, $response, $args){
+        /*
+        Esta funcion recibe un get request y devuelve todos los juegos
+        */
         $db = new DB();
-        $body = json_decode($request->getBody(), true);
-        if (!isset($body['nombre'], $body['url'], $body['imagen'], $body['tipo_imagen'], $body['descripcion'])) throw new Exception("no se recibieron todos los parametros", 400);
-    	$query = $db->prepare('INSERT INTO juegos (nombre, imagen, tipo_imagen, descripcion, url, id_genero, id_plataforma) VALUES (:v1,:v2,:v3,:v4,:v5,:v6,:v7)');	
-	    $params = array(
-        	':v1' => $body['nombre'],
-        	':v2' => $body['imagen'],
-            ':v3' => $body['tipo_imagen'],
-            ':v4' => $body['descripcion'],
-            ':v5' => $body['url'],
-            ':v6' => $body['id_genero'],
-            ':v7' => $body['id_plataforma']
-		    );
+        $respuesta = $db->makeQuery('SELECT * FROM juegos')->fetchAll();
+        $response->getBody()->write(json_encode($respuesta));
+        return $response->withStatus(200);
+    }  
+    public function juegos (Request $request, Response $response, $args){
+        /*
+        Esta función recibe un GET request con parámetros para buscar juego.
+        Es obligatorio que exista al menos uno de estos tres parámetros:
+            -genero(str).
+            -plataforma(str).
+            -nombre(str).
+        Es opcional el siguiente parámetro:
+            -orden(boolean): orden predefinido = ASC.
+        */
+
+        $db = new DB();
         try {
-            $db->makeQuery($query, $params);
+            $genero = $request->getQueryParams()['genero'] ?? null;
+            $nombre = $request->getQueryParams()['nombre'] ?? null;
+            $plataforma = $request->getQueryParams()['plataforma'] ?? null;
+            if ($genero === null && $plataforma === null && $nombre === null) throw new Exception("se debe dar un parametro (genero o plataforma o nombre)", 400);
+            $asc = $request->getQueryParams()['asc']?? false;
+            $datos = [];
+            $query = "SELECT * FROM juegos WHERE 1=1 ";
+            if($genero != null){
+                $query.="AND id_genero = ?";
+                array_push($datos,$genero);
+            }
+            if($nombre != null){
+                $query .= "AND nombre like ?";
+                $nombre = "%".$nombre."%";
+                array_push($datos, $nombre);
+            }
+            if($plataforma!= null){
+                $query.="AND plataforma =?";
+                array_push($datos, $plataforma);
+            }
+            if($asc)$query.=" ORDER BY nombre ASC ";
+            $respuesta = $db->makeQuery($query, $datos)->fetchAll();
+            if(count($respuesta) === 0)throw new Exception("No se encontro el juego", 404);
+            $response->getBody()->write(json_encode($respuesta));
+            $db->close();
             return $response->withStatus(200);
         }
-        catch(Exception $e) {
+        catch (Exception $e) {
+            $response->getBody()->write($e->getMessage());
+            $db->close();
+            return $response->withStatus(404);
+        }
+    }
+    public function createJuego ($request, $response, $args) {
+        /*
+        Esta función recibe un POST request para agregar un juego nuevo.
+        Respeta y valida las condiciones previamente establecidas en la Entrega nº1 en cuanto
+        a la creación de un nuevo juego vía formulario de html.
+        Por tanto, es obligatorio que la request contenga los siguientes campos:
+            nombre(str),
+            imagen(blob en base64),
+            tipo_imagen(str): debe ingresarse un tipo válido,
+            id_plataforma(int): debe ser un id válido de una plataforma existente
+        Los siguientes campos son opcionales:
+            descripcion(str): no más de 255 caracteres
+            url(str): no más de 80 caracteres
+            id_genero(): debe ser un id válido de un genero existente
+        Si la creación del juego es exitosa se imprime un mensaje junto con un Status200 en la response.
+        */
+        $db = new DB();
+        $body = json_decode($request->getBody(), true);
+        try {
+            if (!isset($body['nombre'], $body['url'], $body['imagen'], $body['tipo_imagen'], $body['descripcion'], $body['id_genero'], $body['id_plataforma'])) throw new Exception("no se recibieron todos los parametros", 400);
+            $params = array(
+                ':v1' => $body['nombre'],
+                ':v2' => $body['imagen'],
+                ':v3' => $body['tipo_imagen'],
+                ':v4' => $body['descripcion'],
+                ':v5' => $body['url'],
+                ':v6' => $body['id_genero'],
+                ':v7' => $body['id_plataforma']
+            );
+            
+            $query = 'INSERT INTO juegos (nombre, imagen, tipo_imagen, descripcion, url, id_genero, id_plataforma) VALUES (:v1,:v2,:v3,:v4,:v5,:v6,:v7)';
+            $db->makeQuery($query,$params);
+            return $response->withStatus(200);
+        }
+        catch (Exception $e) {
             $response->getBody()->write($e->getMessage());
             return $response->withStatus($e->getCode());
         }
     }
 
     //* actualizar juego con id 
-    public function update($request, $response, $args) {
+    public function updateJuegos($request, $response, $args)
+    {
         $db = new DB();
         try {
             if (!is_numeric($args['id'])) throw new Exception("El id debe ser numerico", 400);
             if (!isset($args['id'])) throw new Exception("No se recibio el id para hacer el update", 400);
             if (!$db->existsIn('juegos', $args['id'])) throw new Exception("No se encontro el id: '" . $args['id'] . "'", 404);
             $body = json_decode($request->getBody(), true);
-
             $query = "UPDATE juegos SET "; // vamos a ir generando la query de a partes
             $bindings = [];
             foreach ($body as $field => $value) { //este for each mapea bindings con campos ingresados 
@@ -49,18 +117,18 @@ class JuegosController{
             $query = rtrim($query, ', '); // elimina la última coma de la query agregada en la última iteración del foreach
             $query .= " WHERE id = :id";
             $bindings[':id'] = $args['id'];
-
-            $db->makeQuery($query, $bindings); //makeQuery prepara la $query y luego la ejecuta con los $bindings
-        }
+            $db->makeQuery($query, $bindings)->fetchAll(); //makeQuery prepara la $query y luego la ejecuta con los $bindings
+            $response->getBody()->write("Se actualizo bien");
+            return $response->withStatus(200);
+        } 
         catch (Exception $e) {
             $response->getBody()->write($e->getMessage());
-            return $response->withStatus($e->getCode());
+            return $response->withStatus(404);
         }
     }
-    //*delete
     public function delete($request, $response, $args){
         $db = new DB();
-        try{
+        try {
             $body = json_decode($request->getBody(), true);
             $result = $db->makeQuery("SELECT * from juegos where id = '" . $body['id'] . "'");
             if($result->rowCount() === 0) throw new Exception("No existe el id", 400);
@@ -68,61 +136,9 @@ class JuegosController{
             $result = $db->makeQuery("DELETE FROM juegos where id = '".$body['id']."'");
             return $response;
         }
-        catch(Exception $e){
+        catch (Exception $e) {
             $response->getBody()->write($e->getMessage());
             return $response->withStatus(400);
-        }
-    }
-    // todos los juegos 
-    public function list($request, $response, $args){
-        $db = new DB();
-        try {
-            $juegos = $db->makeQuery('SELECT * FROM juegos')->fetchAll();
-            //forma de enviar las exepciones
-            if (sizeof($juegos) === 0)throw new Exception("No hay juegos", 404);
-            $response->getBody()->write(json_encode($juegos));
-            return $response->withStatus(200);
-	    } 
-        catch (Exception $e) {
-            $response->getBody()->write($e->getMessage());
-            return $response->withStatus(404);
-        }
-    }    
-    public function search($request, $response, $args) {
-        /* Esta función debe recibir al menos 1 parámetro de búsqueda. Si el único parámetro de búsqueda es el orden 
-        se debe solicitar que se inserte otro parámetro de búsqueda adicional */
-        $db = new DB();
-        try {
-            $body = json_decode($request->getBody(), true);
-            $query = "SELECT * FROM juegos WHERE ";
-            $bindings = [];
-            $length = count($body);
-            if ($length == 1) {
-                if (isset($body['orden'])) throw new Exception("Ingrese al menos otro parámetro de búsqueda adicional", 400);
-            }
-            /* Preguntar si la última clave es el orden o no. Si no hay orden, el order by no se agrega
-            Si hay orden se bindea y se agrega el fragmento de query con el ORDER BY.*/            
-            for ($i = 0; $i < $length - 1; $i++) {
-                $query .= "{$body[$i]} = :{$body[$i]}, "; // clave = binding que tendrá el valor de dicha clave
-                $bindings[":{$body[$i]}"] = $body[$i]; // se realiza dicho binding
-            }
-            if (array_key_exists('orden', $body)) {
-                $query = rtrim($query, ', ');
-                $lastValue = $body['orden'];
-                $query .= " ORDER BY :{$lastValue}";
-                $bindings[":{$body['orden']}"] = $body['orden'];
-            }
-            else {
-                $query .= "{$body[$i]} = :{$body[$i]}"; // igual que en el for pero sin al coma adicional
-                $bindings[":{$body[$i]}"] = $body[$i];  
-            }
-            $db->makeQuery($query, $bindings);
-    
-            return $response->withStatus(200);
-        } 
-        catch (Exception $e) {
-            $response->getBody()->write($e->getMessage());
-            return $response->withStatus(404);
         }
     }
 }
--- index.php
+++ index.php
@@ -23,16 +23,22 @@ $app->delete('/plataformas/{id}', '\App\src\Models\PlataformaController:delete')
 
 $app->get('/juegosall', '\App\src\Models\PlataformaController:juegosAll');
 /* m) Buscar juegos: implementar un endpoint que permita buscar juegos por nombre, plataforma y género. 
-El endpoint deberá aceptar un nombre, un id de género, un id de plataforma y un orden por nombre (ASC o DESC)*/
+      El endpoint deberá aceptar un nombre, un id de género, un id de plataforma y un orden por nombre (ASC o DESC)*/
 
 $app->get('/juegos', '\App\src\Models\PlataformaController:juegos');
-//endpoints Juegos
+/* Este endpoint recibe un GET request con parámetros para buscar juego:
+    Es obligatorio que exista al menos uno de estos tres parámetros:
+    -genero(str),
+    -plataforma(str),
+    -nombre(str),
+    Es opcional el siguiente parámetro:
+    -orden(boolean): orden predefinido = ASC*/
 
-/*
-i) Crear un nuevo juego: implementar un endpoint para crear un nuevo juego en la tabla de juegos.
+/* i) Crear un nuevo juego: implementar un endpoint para crear un nuevo juego en la tabla de juegos.
 El endpoint debe permitir enviar el nombre, imagen, descripción, plataforma, URL y género.
 */
 $app->post('/juegos', '\App\src\Models\PlataformaController:createJuego');
+
 /*j) Actualizar información de un juego: implementar un endpoint para actualizar la información de un juego
 existente en la tabla de juegos. El endpoint debe permitir enviar el id y los campos que se quieran actualizar*/
 $app->put('/juegos/{id}', '\App\src\Models\PlataformaController:updateJuegos');
